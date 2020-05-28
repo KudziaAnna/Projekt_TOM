4be285c12f892b8653495c4dfa575d30d66915e8
KudziaAnna <44755987+KudziaAnna@users.noreply.github.com>
2020-05-28 09:41:24 +0200

    Add files via upload

--- training_network.py
+++ training_network.py
@@ -9,15 +9,19 @@ from segmentation_models import Unet
 from segmentation_models.losses import CategoricalCELoss
 from segmentation_models.metrics import FScore
 from keras.preprocessing.image import ImageDataGenerator
+import tensorflow as tf
 
-batch_size = 5
-target_size = (256,256)
-num_dev_samples =5
+tf.config.experimental.list_physical_devices('GPU')
 
-path_train_X = 'img_cache/vol/train_X'
-path_dev_X = 'img_cache/vol/dev_X'
-path_train_Y = 'img_cache/seg/train_Y'
-path_dev_Y = 'img_cache/seg/dev_Y'
+batch_size = 200
+target_size = (128,128)
+num_dev_samples =2896
+num_train_samples = 38782 
+
+path_train_X = 'images/train/VOL'
+path_dev_X = 'images/dev/VOL'
+path_train_Y = 'images/train/SEG'
+path_dev_Y = 'images/dev/SEG'
 
 image_train_datagen =  ImageDataGenerator()
 mask_train_datagen = ImageDataGenerator()
@@ -26,24 +30,25 @@ mask_dev_datagen = ImageDataGenerator()
 
 seed =1
 image_train_generator = image_train_datagen.flow_from_directory(
-    path_train_X,
+    directory = path_train_X,
     target_size = target_size,
     batch_size = batch_size,
     class_mode=None,
     seed=seed)
 mask_train_generator = mask_train_datagen.flow_from_directory(
-    path_train_Y,
+    directory = path_train_Y,
     target_size = target_size,
     batch_size = batch_size,
     class_mode=None,
     seed=seed)
 image_dev_generator = image_dev_datagen.flow_from_directory(
-    path_dev_X,
+    directory = path_dev_X,
     target_size = target_size,
     batch_size = batch_size,
     class_mode=None,
     seed=seed)
 mask_dev_generator = mask_dev_datagen.flow_from_directory(
+    directory = path_dev_Y,
     target_size = target_size,
     batch_size = batch_size,
     class_mode=None,
@@ -53,16 +58,16 @@ train_generator = zip(image_train_generator, mask_train_generator)
 dev_generator = zip(image_dev_generator, mask_dev_generator)
 
 #%%
-model = Unet('resnet34',encoder_weights='imagenet', classes = 3, input_shape=(256, 256, 3),)
+model = Unet('resnet34',encoder_weights='imagenet', classes = 3, input_shape=(128, 128, 3),)
 loss = CategoricalCELoss()
 metrics = FScore()
 model.compile('Adam', loss=loss, metrics=[metrics])
 model.summary()
 
 model.fit_generator(train_generator,
-                    steps_per_epoch = 40,
+                    steps_per_epoch = num_train_samples/batch_size,
                     epochs = 10,
                     validation_data = dev_generator,
                     validation_steps = num_dev_samples/batch_size)
 
-#model.save_weights('nn_weights.h5')
+model.save_weights('nn_weights.h5')

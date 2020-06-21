e846f38611085ea6d3ee6552b422edd374c32d66
MagdalenaBialik <magdalena.bialik@gmail.com>
2020-06-21 13:59:56 +0200

    Removed data augmentation from test files, added project summary PDF file - last commit

--- build_images.py
+++ build_images.py
@@ -20,8 +20,8 @@ imageio.core.util._precision_warn = silence_imageio_warning
 
 def save_vol_and_seg_from_cid(case_id,base_dir,sub_dir,hu_min=DEFAULT_HU_MIN,hu_max=DEFAULT_HU_MAX):
     if sub_dir=='test':
-        volpath = base_dir + "/" + sub_dir + '/case_' + str(case_id) + "/VOL/"
-        segpath = base_dir + "/" + sub_dir + '/case_' + str(case_id) + "/SEG/"
+        volpath = base_dir + "/" + sub_dir + '/case_' + str(case_id) + "/VOL/vol/"
+        segpath = base_dir + "/" + sub_dir + '/case_' + str(case_id) + "/SEG/seg/"
     else:
         volpath=base_dir + "/" + sub_dir + '/VOL/vol/'
         segpath = base_dir + "/" + sub_dir + '/SEG/seg/'
@@ -40,33 +40,37 @@ def save_vol_and_seg_from_cid(case_id,base_dir,sub_dir,hu_min=DEFAULT_HU_MIN,hu_
 
     for i in tqdm(range(vol.shape[0])):
         case_str="case{}_{:05d}.png".format(case_id,i)
-        flip_case_str = "case{}_{:05d}_flipped.png".format(case_id, i)
         volimgpath = volpath + case_str
         segimgpath = segpath + case_str
-        flip_volimgpath = volpath + flip_case_str
-        flip_segimgpath = segpath + flip_case_str
-        flipped_vol = vol[i][:, ::-1]
-        flipped_seg = seg[i][:, ::-1]
 
         if case_str in ready_vol:
             print(volpath+case_str+' already satisfied')
         else:
             plt.imsave(str(volimgpath), vol[i],cmap='gray')
 
-        if flip_case_str in ready_vol:
-            print(volpath + flip_case_str + ' already satisfied')
-        else:
-            plt.imsave(str(flip_volimgpath), flipped_vol, cmap='gray')
 
-        if case_str in ready_seg:
-            print(segpath+case_str+' already satisfied')
-        else:
-            plt.imsave(str(segimgpath), seg[i],cmap='gray')
 
-        if flip_case_str in ready_seg:
-            print(segpath + flip_case_str + ' already satisfied')
-        else:
-            plt.imsave(str(flip_segimgpath), flipped_seg, cmap='gray')
+        if sub_dir != 'test':
+            if case_str in ready_seg:
+                print(segpath + case_str + ' already satisfied')
+            else:
+                plt.imsave(str(segimgpath), seg[i], cmap='gray')
+
+            flip_case_str = "case{}_{:05d}_flipped.png".format(case_id, i)
+            flip_volimgpath = volpath + flip_case_str
+            flip_segimgpath = segpath + flip_case_str
+            flipped_vol = vol[i][:, ::-1]
+            flipped_seg = seg[i][:, ::-1]
+
+            if flip_case_str in ready_vol:
+                print(volpath + flip_case_str + ' already satisfied')
+            else:
+                plt.imsave(str(flip_volimgpath), flipped_vol, cmap='gray')
+
+            if flip_case_str in ready_seg:
+                print(segpath + flip_case_str + ' already satisfied')
+            else:
+                plt.imsave(str(flip_segimgpath), flipped_seg, cmap='gray')
 
 
 if __name__ == "__main__":
